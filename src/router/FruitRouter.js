a385783a6a5f661bb2083330cd0fa9fef7ee14da
Krunal Jumde <KrunalJumde24@gmail.com>
2024-02-07 22:35:22 +0530

    Finished the AddVegetable API

--- src/router/FruitRouter.js
+++ src/router/FruitRouter.js
@@ -4,6 +4,7 @@ const { mysqlconnection } = require("../service/mysqlConnection");
 const FruitRouter = Router({ strict: true });
 
 FruitRouter.post("/addFruit", (req, res) => {
+  console.log(req.body);
   res.status(201).send("Fruit Added.");
 });
 
--- src/router/VegeRouter.js
+++ src/router/VegeRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { mysqlconnection } = require("../service/mysqlConnection");
+const { Result } = require("express-validator");
 
 const VegeRouter = Router({ strict: true });
 
@@ -12,7 +13,38 @@ VegeRouter.get("/getVegtableDetails", (req, res) => {
 
 VegeRouter.post("/addVegetable", (req, res) => {
   console.log(req.body);
-  res.status(201).send('Vegetable Added.')
+  let obj = req.body;
+  mysqlconnection.query(
+    "INSERT INTO dukaan_db.product_vegetable (`productId`,`productName`,`productActualPrice`,`productDiscountedPrice`,`productQuantity`,`productQuantityType`,`productImageName`)  VALUES (?,?,?,?,?,?,?)",
+    [
+      'v100',
+      obj.productName,
+      obj.productActualPrice,
+      obj.productDiscountedPrice,
+      obj.productQuantity,
+      obj.productQuantityType,
+      "product_image",
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal server error.");
+      } else {
+        console.log(result);
+        res.status(201).send("Vegetable Added.");
+      }
+    }
+  );
 });
+
+// VegeRouter.get("/getUniqueProductId", (req, res) => {
+//   mysqlconnection.query(
+//     "SELECT COUNT(*) as 'COUNT' FROM product_vegetable",
+//     (err, result) => {
+      
+//       res.status(200).send(JSON.stringify(result[0]));
+//     }
+//   );
+// });
 
 module.exports = { VegeRouter };
